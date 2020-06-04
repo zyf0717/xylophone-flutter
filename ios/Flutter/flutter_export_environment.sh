4518e77aed850bb8d3a44338a17326b031c8cb61
zyf0717 <zyf0717@gmail.com>
2020-06-04 13:05:07 +0800

    Initial upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/zyf0717/git/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/zyf0717/AndroidStudioProjects/xylophone-flutter"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/home/zyf0717/git/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int x) {
+    final player = AudioCache();
+    player.play('note$x.wav');
+  }
+
+  Expanded buildKey(Color color, int x) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        width: double.infinity,
+        color: color,
+        child: FlatButton(
+          child: null,
+          onPressed: () {
+            playSound(x);
+          },
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.teal, 5),
+              buildKey(Colors.blue, 6),
+              buildKey(Colors.purple, 7),
+            ],
+          ),
         ),
       ),
     );
